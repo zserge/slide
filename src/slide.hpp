9b59d02384089af7a9563721db4b45d63b377a95
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2017-11-23 13:59:00 +0200

    slide: refactor cairo renderer

--- src/slide.hpp
+++ src/slide.hpp
@@ -187,12 +187,173 @@ static void render(Page &page, Slide &slide, Color fg, Color bg) {
 
 namespace slide {
 
+namespace cairo {
+
+static void set_font(cairo_t *cr, Style style, float scale) {
+  switch (style) {
+  case Normal:
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+			   CAIRO_FONT_WEIGHT_NORMAL);
+    break;
+  case Strong:
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+			   CAIRO_FONT_WEIGHT_BOLD);
+    break;
+  case Header:
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+			   CAIRO_FONT_WEIGHT_BOLD);
+    scale = scale * 1.6f;
+    break;
+  case Monospace:
+    cairo_select_font_face(cr, "Monospace", CAIRO_FONT_SLANT_NORMAL,
+			   CAIRO_FONT_WEIGHT_NORMAL);
+    break;
+  }
+  cairo_set_font_size(cr, 16 * scale);
+}
+
+static void set_source_color(cairo_t *cr, Color color) {
+  double r = ((color >> 16) & 0xff) / 255.0;
+  double g = ((color >> 8) & 0xff) / 255.0;
+  double b = ((color)&0xff) / 255.0;
+  double a = ((color >> 24) & 0xff) / 255.0;
+  cairo_set_source_rgba(cr, r, g, b, a);
+}
+static int text_height(cairo_t *cr, Style style, float scale) {
+  cairo_font_extents_t fe;
+  set_font(cr, style, scale);
+  cairo_font_extents(cr, &fe);
+  return (int)(fe.height);
+}
+
+static int text_width(cairo_t *cr, const std::string &text, Style style,
+		      float scale) {
+  cairo_text_extents_t te;
+  set_font(cr, style, scale);
+  cairo_text_extents(cr, text.c_str(), &te);
+  return (int)(te.x_advance);
+}
+
+static void bg(cairo_t *cr, int w, int h, Color color) {
+  set_source_color(cr, color);
+  cairo_rectangle(cr, 0, 0, w, h);
+  cairo_fill(cr);
+}
+
+static void text(cairo_t *cr, const std::string &text, Color color, int x,
+		 int y, Style style, float scale) {
+  cairo_font_extents_t fe;
+  set_source_color(cr, color);
+  set_font(cr, style, scale);
+  cairo_font_extents(cr, &fe);
+  cairo_move_to(cr, x, y + fe.ascent);
+  cairo_show_text(cr, text.c_str());
+}
+} // namespace cairo
+
+namespace base64 {
+
+static const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+					"abcdefghijklmnopqrstuvwxyz"
+					"0123456789+/";
+std::string encode(unsigned char const *bytes_to_encode, unsigned int in_len) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] =
+	  ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] =
+	  ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+      for (i = 0; (i < 4); i++) {
+	ret += base64_chars[char_array_4[i]];
+      }
+      i = 0;
+    }
+  }
+  if (i) {
+    for (j = i; j < 3; j++) {
+      char_array_3[j] = '\0';
+    }
+    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+    char_array_4[1] =
+	((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+    char_array_4[2] =
+	((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+    char_array_4[3] = char_array_3[2] & 0x3f;
+    for (j = 0; (j < i + 1); j++) {
+      ret += base64_chars[char_array_4[j]];
+    }
+    while ((i++ < 3)) {
+      ret += '=';
+    }
+  }
+  return ret;
+}
+
+} // namespace base64
+
+class PNG : public Page {
+public:
+  PNG(const int w, const int h) : w(w), h(h) {
+    this->surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w, h);
+    this->cr = cairo_create(this->surface);
+  }
+  ~PNG() {
+    cairo_surface_destroy(this->surface);
+    cairo_destroy(this->cr);
+  }
+
+  int width() const { return this->w; }
+  int height() const { return this->h; }
+  int text_height(Style style, float scale) {
+    return cairo::text_height(this->cr, style, scale);
+  }
+  int text_width(const std::string &text, Style style, float scale) {
+    return cairo::text_width(this->cr, text, style, scale);
+  }
+  void bg(Color color) { cairo::bg(this->cr, this->w, this->h, color); }
+  void text(const std::string &text, Color color, int x, int y, Style style,
+	    float scale) {
+    cairo::text(cr, text, color, x, y, style, scale);
+  }
+
+  void save(const std::string filename) {
+    cairo_surface_write_to_png(this->surface, filename.c_str());
+  }
+
+  std::string dataURI() {
+    std::string b64 = "data:image/png;base64,";
+    cairo_surface_write_to_png_stream(
+	this->surface,
+	[](void *closure, const unsigned char *data, unsigned int length) {
+	  std::string *b64 = (std::string *)closure;
+	  b64->append(base64::encode(data, length));
+	  return CAIRO_STATUS_SUCCESS;
+	},
+	&b64);
+    return b64;
+  }
+
+private:
+  const std::string name;
+  const int w;
+  const int h;
+  cairo_surface_t *surface;
+  cairo_t *cr;
+};
+
 class PDF : public Document {
 public:
-  PDF(const int w, const int h) : w(w), h(h) {
-    this->surface = cairo_pdf_surface_create("out.pdf", w, h);
+  PDF(const std::string name, const int w, const int h) : w(w), h(h) {
+    this->surface = cairo_pdf_surface_create(name.c_str(), w, h);
     this->cr = cairo_create(this->surface);
-    cairo_set_source_rgb(this->cr, 0, 0, 0);
   }
 
   ~PDF() {
@@ -205,69 +366,19 @@ public:
 
   int width() const { return this->w; }
   int height() const { return this->h; }
-
   int text_height(Style style, float scale) {
-    cairo_font_extents_t fe;
-    this->set_font(style, scale);
-    cairo_font_extents(this->cr, &fe);
-    return (int)(fe.height);
+    return cairo::text_height(this->cr, style, scale);
   }
-
   int text_width(const std::string &text, Style style, float scale) {
-    cairo_text_extents_t te;
-    this->set_font(style, scale);
-    cairo_text_extents(this->cr, text.c_str(), &te);
-    return (int)(te.x_advance);
+    return cairo::text_width(this->cr, text, style, scale);
   }
-
-  void bg(Color color) {
-    this->set_source_color(color);
-    cairo_rectangle(this->cr, 0, 0, this->w, this->h);
-    cairo_fill(this->cr);
-  }
-
+  void bg(Color color) { cairo::bg(this->cr, this->w, this->h, color); }
   void text(const std::string &text, Color color, int x, int y, Style style,
 	    float scale) {
-    cairo_font_extents_t fe;
-    this->set_source_color(color);
-    this->set_font(style, scale);
-    cairo_font_extents(this->cr, &fe);
-    cairo_move_to(this->cr, x, y + fe.ascent);
-    cairo_show_text(this->cr, text.c_str());
+    cairo::text(cr, text, color, x, y, style, scale);
   }
 
 private:
-  void set_font(Style style, float scale) {
-    switch (style) {
-    case Normal:
-      cairo_select_font_face(this->cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-			     CAIRO_FONT_WEIGHT_NORMAL);
-      break;
-    case Strong:
-      cairo_select_font_face(this->cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-			     CAIRO_FONT_WEIGHT_BOLD);
-      break;
-    case Header:
-      cairo_select_font_face(this->cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-			     CAIRO_FONT_WEIGHT_BOLD);
-      scale = scale * 1.6f;
-      break;
-    case Monospace:
-      cairo_select_font_face(this->cr, "Monospace", CAIRO_FONT_SLANT_NORMAL,
-			     CAIRO_FONT_WEIGHT_NORMAL);
-      break;
-    }
-    cairo_set_font_size(this->cr, 16 * scale);
-  }
-
-  void set_source_color(Color color) {
-    double r = ((color >> 16) & 0xff) / 255.0;
-    double g = ((color >> 8) & 0xff) / 255.0;
-    double b = ((color)&0xff) / 255.0;
-    double a = ((color >> 24) & 0xff) / 255.0;
-    cairo_set_source_rgba(this->cr, r, g, b, a);
-  }
-
   const int w;
   const int h;
   cairo_surface_t *surface;
